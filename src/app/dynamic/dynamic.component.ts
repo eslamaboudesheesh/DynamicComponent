12966c88a1a335d2500ac0d8fd16a3ff6a71cff7
Mohammed Hussein <t-emhussein@EFG-Hermes.com>
2020-06-19 05:06:11 +0200

    add filter in gride and Paging

--- src/app/dynamic/dynamic.component.ts
+++ src/app/dynamic/dynamic.component.ts
@@ -7,6 +7,7 @@ import {
   Output,
   EventEmitter,
 } from '@angular/core';
+import { process, State } from '@progress/kendo-data-query';
 
 @Component({
   selector: 'app-dynamic',
@@ -14,18 +15,25 @@ import {
   styleUrls: ['./dynamic.component.css'],
 })
 export class DynamicComponent implements OnInit {
-  public gridData = [];
+  public gridData: any;
+  public gridDataResult: any;
   public gridField = [];
+  public gridWithoutData: boolean = false;
 
   public childgridData = [];
   public childgridField = [];
   public UserId: any;
+  public state: State = {
+    skip: 0,
+    take: 10,
+  };
   @Input() set componentData(data: { component: any; gridField: any }) {
     if (!data) {
       return;
     }
-    this.gridData = data.component;
+    this.gridDataResult = data.component;
     this.gridField = data.gridField;
+    this.loadItem(this.gridDataResult);
   }
 
   @Input() set ChildData(data: {
@@ -42,14 +50,55 @@ export class DynamicComponent implements OnInit {
   }
 
   @Output() update = new EventEmitter();
+  @Output() onFilter = new EventEmitter();
 
   constructor() {}
 
   ngOnInit(): void {}
-  Update(dataItem, rowIndex) {
+
+  loadItem(gridDataRequest: any) {
+    this.gridData = process(gridDataRequest, this.state);
+    if (this.gridData) {
+      this.gridWithoutData = true;
+    }
+  }
+
+  dataStateChange(state: State) {
+    this.state = state;
+    var result = process(this.gridDataResult, this.state);
+    this.gridData = result;
+  }
+
+  ShowAction(dataItem, rowIndex) {
     if (dataItem.userId == this.UserId) {
       alert(`it's the same  user Id ${this.UserId} every user have 10 posts `);
     }
     this.update.emit({ dataItem, rowIndex });
+  }
+
+  onFilterForGrid(inputValue: string): void {
+    if (inputValue.length == 0) {
+      this.loadItem(this.gridDataResult);
+    }
+    let result = process(this.gridDataResult, {
+      filter: {
+        logic: 'or',
+        filters: [
+          {
+            field: this.gridField[1].field,
+            operator: 'contains',
+            value: inputValue,
+          },
+          {
+            field: this.gridField[2].field,
+            operator: 'contains',
+            value: inputValue,
+          },
+        ],
+      },
+    }).data;
+    this.gridData = result;
+    this.state.skip = 0;
+    this.state.take = 10;
   }
 }
